2562b377d39ab1fcd6e9ddf245924a1709567067
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 18:02:06 +0530

    Evenodd program

---
+++ Evenodd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evenoddprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program for even or odd no.
+            int num = 11;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
