3cb91a4c2cc20d05bb3337717ff91a406f2b6bdc
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 18:09:03 +0530

    Armstrongno Program

---
+++ Armstrong.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Armstrongno_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program for Armstrong no.
+
+            int num;
+            Console.WriteLine("Enter the no");
+            num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int rem;
+            int n = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                sum = sum + rem * rem * rem;
+                num = num / 10;
+            }
+            if (n == sum)
+            {
+                Console.WriteLine("no. is Armstrong");
+            }
+            else
+            {
+                Console.WriteLine("no. is not Armstrong");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
