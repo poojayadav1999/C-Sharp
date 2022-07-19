89a6138141f6ff236158f79afe1aa491614595a7
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 18:12:26 +0530

    Swap two no using third variable program

---
+++ swapusingthirdvar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Swaptwonousingthirdvariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program for Swapping two no using third no.
+
+            int a = 10, b = 20, temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("Values after swapping are:");
+            Console.WriteLine("value of a is:" + a);
+            Console.WriteLine("value of b is:" + b);
+            Console.ReadLine();
+        }
+    }
+}
