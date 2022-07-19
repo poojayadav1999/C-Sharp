62c4beced5f3170689796ef0bd68945e2596733f
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 17:59:15 +0530

    while loop program

---
+++ whileloop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Whileloop_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
