dc4e7909013c10607632aa87deb6f64dcdf5a242
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 18:00:50 +0530

    do-while loop program

---
+++ do-while.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace do_whileloop_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do-while loop
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
