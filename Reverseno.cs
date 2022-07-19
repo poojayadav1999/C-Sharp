002292b2b19a0c128f34ba0b98a186fadfe1a1e2
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 18:07:35 +0530

    Reverse no.Program

---
+++ Reverseno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverseno_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program for Reverse no.
+
+            int num;
+            Console.WriteLine("Enter the no");
+            num = int.Parse(Console.ReadLine());
+            int n = num;
+            int rem;
+            int Revnum = 0;
+
+            while (num != 0)
+            {
+                rem = num % 10;
+                Revnum = (Revnum * 10) + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse number is:" + Revnum);
+            Console.ReadLine();
+
+        }
+    }
+}
