ac2ab125431863d649937cb503ca6fed6a46fa3e
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 17:57:46 +0530

    for loop program

---
+++ forloop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace forloop_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
