78944a4c3b2be49cc6ccbbb63550f74963b86a64
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-19 17:56:22 +0530

    If_else ladder Pogram

---
+++ If_elseladder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace If_elseladder_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If_else ladder
+            int z = 20;
+            if (z <= 15)
+            {
+                Console.WriteLine("You cannot drive");
+            }
+            else if (z < 18)
+            {
+                Console.WriteLine("You have a license");
+            }
+            else if (z <= 20)
+            {
+                Console.WriteLine("please you can get license");
+            }
+            else
+            {
+                 Console.WriteLine("You can drive");
+            }
+            Console.ReadLine();
+        }
+    }
+}
